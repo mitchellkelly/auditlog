09c1ab5e4e1edc658cf91e440f21adc682185eae
Mitchell Kelly <mitchellkelly@outlook.com>
2022-04-07 14:06:52 -0400

    Added the option to run the server using tls

--- main.go
+++ main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// variables that will be set to values supplied by the user via the command line
+	var serverPort int
+	var shouldServeTls bool
+
+	flag.IntVar(&serverPort, "p", 80, "The TCP port for the server to listen on")
+	flag.BoolVar(&shouldServeTls, "t", false, "Handle requests using TLS encryption")
+
+	// parse the command line args for flag values
+	flag.Parse()
+
+	var tlsCert string
+	var tlsKey string
+
+	// set the default port to 443 if tls was requested and the server port was not explicitly set
+	if shouldServeTls {
+		var portExplictlySet bool
+
+		// check if the port flag was explicitly set
+		// by iterating over all of the flag values that were set
+		flag.Visit(func(f *flag.Flag) {
+			if f.Name == "p" {
+				portExplictlySet = true
+			}
+		})
+
+		if portExplictlySet == false {
+			serverPort = 443
+		}
+
+		// get the cert and key values from env variables
+		tlsCert = os.Getenv("AUDIT_LOG_TLS_CERT")
+		tlsKey = os.Getenv("AUDIT_LOG_TLS_KEY")
+	}
+
 	// create a new http multiplexer for handling http requests
 	var mux = http.NewServeMux()
 
@@ -13,9 +50,17 @@ func main() {
 
 	// create an http server for serving requests using the wrapped multiplexer we created
 	var server = http.Server{
+		Addr: fmt.Sprintf(":%d", serverPort),
 		Handler: serveHandler,
 	}
 
-	var serverError = server.ListenAndServe()
+	// start the server
+	var serverError error
+	if shouldServeTls {
+		serverError = server.ListenAndServeTLS(tlsCert, tlsKey)
+	} else {
+		serverError = server.ListenAndServe()
+	}
+
 	_ = serverError
 }
